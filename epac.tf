33285d66c36c278502c384368aab6740b089a600
Bhuvana-1500 <bhuvanamungara15@gmail.com>
2025-02-14 08:46:42 +0530

    Update epac.tf

--- epac.tf
+++ epac.tf
@@ -23,13 +23,13 @@ provider "azurerm" {
   client_id = "9c2e5200-9c57-46ad-aacd-b087b4c1c5be"
   client_secret = "4Qr8Q~oMYsyr4944u2D0a-W6Op7IScyoqoLBUdfi"
   tenant_id = "30bf9f37-d550-4878-9494-1041656caf27"
-  subscription_ids = "13ba43d9-3859-4c70-9f8d-182debaa038b"
+  subscription_id = "13ba43d9-3859-4c70-9f8d-182debaa038b"
 }
 
 resource "azurerm_management_group" "root" {
   name                       = "mgmt0"
   display_name               = "mgmt0"
-  subscription_id           = ["b1af8833-cc76-47d5-ac29-4f7d63cdb243"]
+  subscription_ids           = ["b1af8833-cc76-47d5-ac29-4f7d63cdb243"]
 }
 
 resource "azurerm_policy_definition" "standard_naming_convention_policy" {
