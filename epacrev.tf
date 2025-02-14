57deeb494564cef1295676a721e3fcf04070b4fa
Bhuvana-1500 <bhuvanamungara15@gmail.com>
2025-02-14 10:54:51 +0530

    Update epacrev.tf

--- epacrev.tf
+++ epacrev.tf
@@ -2,3 +2,7 @@ data "azurerm_management_group_policy_assignment" "mgmtrev" {
   name               = "mgmt0"
   management_group_id = "/providers/Microsoft.Management/managementGroups/mgmt0"
 }
+
+output "policy_assignments" {
+  value = data.azurerm_management_group_policy_assignment.mgmtrev
+}
